ae8e72cfabab5546e6abda425474ea574ced6c66
Platformer10101 <platformer10101@gmail.com>
2024-08-15 10:30:21 -0400

    Add files via upload

---
+++ a4.py
@@ -0,0 +1,8 @@
+import datetime
+curremt_time = datetime.datetime.now()
+
+print("Time now at eastern standard time is : ", end = "")
+print(curremt_time)
+
+import calendar
+print("\n", calendar.calendar(2024))
